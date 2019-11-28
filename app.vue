a710039650c76f7f7c8067b4542ad6c35d5f2097
layoung <184592775@qq.com>
2019-11-28 22:00:08 +0800

    vue note

---
+++ app.vue
@@ -0,0 +1,30 @@
+<!-- 组件模板   在template中写dom元素 -->
+<template>
+    <div>i am a div</div>
+</template>
+
+<!-- 将模板以外的东西都写在script里面 -->
+<script>
+export default {
+    data() {
+        return {
+            msg : 'hello world'
+        }
+    }
+}
+</script>
+
+<!-- style写css样式的 -->
+<style>
+    div {
+        width: 100px;
+        height: 100px;
+        color: #fff;
+        background-color: red;
+    }
+</style>
+
+<!-- 使用vue serve app.vue(组件名)可以运行
+     如果组件名就是App.vue的话,就可以直接用vue serve运行
+ -->
+
